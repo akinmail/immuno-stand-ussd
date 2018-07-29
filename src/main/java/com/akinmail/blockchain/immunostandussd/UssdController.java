bea2bac12c3b288032d23ab42746b8974389b96e
Akindele Akinyemi <akbumper@gmail.com>
2018-07-29 07:10:05 +0100

    changes

--- src/main/java/com/akinmail/blockchain/immunostandussd/UssdController.java
+++ src/main/java/com/akinmail/blockchain/immunostandussd/UssdController.java
@@ -1,13 +1,13 @@
 package com.akinmail.blockchain.immunostandussd;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ussd")
 public class UssdController {
 
-    @RequestMapping(value="", method=RequestMethod.POST, headers = {"content-type=application/x-www-form-urlencoded;charset=UTF-8"})
-    public String message(@RequestBody UssdRequest ussdRequest) {
+    @RequestMapping(value="/ussd", method=RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String message(UssdRequest ussdRequest) {
         if(ussdRequest.getText().equals("")){
             return "CON Hello \n"+"1. Register Child \n"+ "2. Immunize Child \n";
         }
